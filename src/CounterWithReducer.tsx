f82ddfb90a002247d9a322523e79ea421e5ea193
nagata03 <nagata.job@gmail.com>
2020-12-06 06:44:53 +0900

    learn useReducer basic

--- src/CounterWithReducer.tsx
+++ src/CounterWithReducer.tsx
@@ -1,25 +1,32 @@
 import React, { useReducer } from 'react';
 
-const initialState = {count: 0};
+type StateType = { count: number };
 
-const reducer = (state: any, action: any) => {
+const initialState: StateType = { count: 0 };
+
+type ActionType = { type: 'decrement' | 'increment' | 'reset' };
+
+const reducer = (state: StateType, action: ActionType): StateType | never => {
   switch (action.type) {
     case 'increment':
-      return {count: state.count + 1};
+      return { count: state.count + 1 };
     case 'decrement':
-      return {count: state.count - 1};
+      return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
     default:
       throw new Error();
   }
-}
+};
 
 export const CounterWithReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       Count: {state.count}
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
-}
+};
